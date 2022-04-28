2f8256d0eb8825f8f75e0e3e4daed2b6a408f4d0
David Almeida <contato.davidalmeida@outlook.com>
2022-04-28 20:54:56 -0300

    Add: Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import Container from "../Container";
+import { StyledFooter } from "./styles";
+
+
+const Footer = () => {
+  return(
+    <StyledFooter>
+      <Container>
+
+      </Container>
+    </StyledFooter>
+  )
+}
+
+export default Footer;

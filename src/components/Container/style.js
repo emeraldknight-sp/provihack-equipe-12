d12c8f0dd3242658a5a9c90af6ee615f82111c9d
David Almeida <contato.davidalmeida@outlook.com>
2022-04-28 20:54:45 -0300

    Add: Container component

---
+++ src/components/Container/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  width: 80%;
+  margin: 0 auto;
+`

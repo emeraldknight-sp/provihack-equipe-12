d8855e0d805a8c19c81e796be8194d67d4222f1d
David Almeida <contato.davidalmeida@outlook.com>
2022-04-29 12:55:06 -0300

    Update: removing ui material buttons, adding content to user profile component

--- src/pages/UserProfile/styles.js
+++ src/pages/UserProfile/styles.js
@@ -6,19 +6,34 @@ export const StyledMenu = styled.ul`
   justify-content: space-between;
   align-items: stretch;
   gap: 10px;
+  z-index: 1;
   cursor: pointer;
 `
 
 export const StyledImage = styled.img`
-  width: 320px;
+  width: 400px;
+  height: 200px;
+  border: none;
+  border-radius: 5px;
 `
 
 export const StyledSection = styled.section`
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    flex: 1 0 30%;
-    height: 327px;
+    flex: 300px 0 1;
+    gap: 20px;
+    transition: transform 0.2s;
+    height: 420px;
+
+    h3 {
+      width: 100%;
+      font-size: var(--fontsize-h3);
+      padding: 20px;
+      border: 2px solid;
+      border-radius: 35px;
+    }
 
     p {
       height: 100px;
@@ -31,4 +46,9 @@ export const StyledSection = styled.section`
       justify-content: center;
       align-items: center;
     }
+
+    &:hover {
+    transform: scale(1.1);
+    cursor: pointer;
+  }
 `

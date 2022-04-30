9e45b7a8853ba2c6388175b4bb01ecaf0210c289
David Almeida <contato.davidalmeida@outlook.com>
2022-04-30 19:56:12 -0300

    Update: organizing imports and redirecting to the user's profile when clicking on the logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,16 @@
-import "./style.css";
-import Logo from "../../assets/img/+Leaf.svg";
-import MenuIcon from "../../assets/img/burguerMenu.svg";
-import CloseIcon from "../../assets/img/close-icon-header/icons8-excluir-64.svg";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CloseIcon from "../../assets/img/close-icon-header/icons8-excluir-64.svg";
+import MenuIcon from "../../assets/img/burguerMenu.svg";
+import Logo from "../../assets/img/+Leaf.svg";
 import SidebarMenu from "../SidebarMenu";
+import "./style.css";
 
 function Header() {
 
   const [ click, setClick ] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleClick = () => {
     if (click !== true) {
@@ -23,7 +26,7 @@ function Header() {
     <header className="header">
       <SidebarMenu click={click} setClick={setClick} />
       <div className="headerBackground">
-        <img className="headerLogo" src={Logo} alt="Logo" />
+        <img className="headerLogo" src={Logo} alt="Logo" onClick={() => navigate("/profile")} />
         <img className="menuBurguer" src={click ? CloseIcon : MenuIcon} alt="Menu" onClick={() => handleClick()}/>
       </div>
     </header>

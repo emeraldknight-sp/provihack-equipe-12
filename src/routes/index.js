e025174b470f79af47079f9e031859e4c78d0010
David Almeida <contato.davidalmeida@outlook.com>
2022-04-30 12:56:27 -0300

    Update: adding new pages, adding react-router-dom 6, commenting codes to change to new navigation syntax

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,25 +3,23 @@ import UserProfile from "../pages/UserProfile";
 import Hangman from "../pages/Hangman";
 import Quiz from "../pages/Quiz";
 import Games from "../pages/Games";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route exact path="/">
+      <Route exact path="/" element={<Login />} />
 
-      </Route>
-      <Route exact path="/login">
+      <Route exact path="/signup" element={<Signup />} />
 
-      </Route>
       <Route exact path="/profile" element={<UserProfile />} />
 
       <Route exact path="/games" element={<Games />} />
 
       <Route exact path="/games/hangman" element={<Hangman />} />
 
-
       <Route exact path="/games/quiz" element={<Quiz />} />
-
 
     </Routes>
   )
